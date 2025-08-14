b30ee23e79c5567fefd313e9efe7af1a78f08c2a
NINOJAS <149446674+NINOJAS@users.noreply.github.com>
2025-08-14 17:33:09 -0500

    Se implementa la funcion mostrarLista  y un botón para eliminar los nombres no deseados

--- app.js
+++ app.js
@@ -6,4 +6,24 @@ function agregarAmigo() {
     alert("por favor, ingrese un nombre");
     return;
   }
-  
+    amigos.push(nombre);
+  document.getElementById("nombreAmigo").value = "";
+  mostrarLista();
+}
+
+function mostrarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  amigos.forEach(function (amigo, indice) {
+    let li = document.createElement("li");
+    li.textContent = amigo + "";
+    
+    let botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "❌";
+    botonEliminar.onclick = function () {
+      eliminarAmigo(indice);
+    };
+    li.appendChild(botonEliminar);  
+    lista.appendChild(li);
+  });
+}
