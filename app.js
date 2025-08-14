44d11e58f83e088c40e5ed993a867df970613771
NINOJAS <149446674+NINOJAS@users.noreply.github.com>
2025-08-14 17:14:54 -0500

    se implementa la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 let amigos = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("nombreAmigo").value.trim();
+  if (nombre === "") {
+    alert("por favor, ingrese un nombre");
+    return;
+  }
+  
