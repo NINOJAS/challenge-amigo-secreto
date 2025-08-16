98edf0e87a202ba6c653294209f965210e9e311d
NINOJAS <149446674+NINOJAS@users.noreply.github.com>
2025-08-15 20:36:55 -0500

    Se remplazan alertas nativas por SweetAlert2 para una mejor experiencia visual

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ let amigos = [];
 function agregarAmigo() {
   let nombre = document.getElementById("nombreAmigo").value.trim();
   if (nombre === "") {
-    alert("por favor, ingrese un nombre");
+     Swal.fire({
+        title: "Campo vacío",
+        text: "Por favor, ingrese un nombre",
+        icon: "warning"
+    });
     return;
   }
   nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
@@ -11,6 +15,13 @@ function agregarAmigo() {
     amigos.push(nombre);
   document.getElementById("nombreAmigo").value = "";
   mostrarLista();
+    Swal.fire({
+    title: `${nombre}`,
+    text: "fue agregad@  a la lista",
+    icon: "success",
+    timer: 1500,
+    showConfirmButton: false
+  });
 }
 
 function mostrarLista() {
@@ -30,19 +41,34 @@ function mostrarLista() {
   });
 }
 function eliminarAmigo(indice) {
+  let eliminado = amigos[indice];
   amigos.splice(indice, 1);
   mostrarLista();
+    Swal.fire({
+    title: `${eliminado}`,
+    text: "fue eliminad@ de la lista",
+    icon: "info",
+    timer: 1500,
+    showConfirmButton: false
+  });
 }
 function sortearAmigo() {
   if (amigos.length === 0) {
-    alert("No hay amigos para sortear");
+    Swal.fire({
+      title: "Oops...",
+      text: "No hay amigos para sortear",
+      icon: "warning"
+    });
     return;
   }
     let indiceAleatorio = Math.floor(Math.random() * amigos.length);
     let amigoSorteado = amigos[indiceAleatorio];
     
-    let resultado = document.getElementById("resultado");
-    resultado.innerHTML = `El amigo secreto es: <strong>${amigoSorteado}</strong>`;
+     Swal.fire({
+    title: "¡Tú Amigo secreto!",
+    html: `El amigo secreto es: <strong>${amigoSorteado}</strong>`,
+    icon: "success"
+  });
 } 
 
 function reiniciarJuego() {
